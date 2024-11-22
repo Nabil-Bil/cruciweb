c759abd88dced6020dafb5151f47c3c919c91474
Nabil-Bil <n.radjai@outlook.fr>
2024-11-22 23:00:50 +0100

    Add ValidationError class to encapsulate field validation errors

---
+++ src/main/java/com/univ/validator/ValidationError.java
@@ -0,0 +1,23 @@
+package com.univ.validator;
+
+public class ValidationError {
+  private String field;
+  private String message;
+
+  public ValidationError(String field, String message) {
+    this.field = field;
+    this.message = message;
+  }
+
+  public String getField() {
+    return field;
+  }
+
+  public String getErrorField() {
+    return field.concat("_error");
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
