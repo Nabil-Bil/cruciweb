eb5dc9e8874ea601d92b939ba3414df799d99e20
Nabil-Bil <n.radjai@outlook.fr>
2024-11-22 23:01:26 +0100

    Implement CSRF protection and user input validation in registration process

--- src/main/java/com/univ/controller/RegisterController.java
+++ src/main/java/com/univ/controller/RegisterController.java
@@ -6,7 +6,10 @@ import com.univ.enums.Role;
 import com.univ.model.User;
 import com.univ.service.UserService;
 import com.univ.service.UserServiceImpl;
+import com.univ.util.CsrfTokenUtil;
 import com.univ.util.ViewResolver;
+import com.univ.validator.UserValidator;
+import com.univ.validator.ValidationError;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -19,20 +22,39 @@ public class RegisterController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     resp.setContentType("text/html");
+    String csrfToken = CsrfTokenUtil.generateCsrfToken(req);
+    req.setAttribute(CsrfTokenUtil.CSRF_SESSION_ATTRIBUTE, csrfToken);
     ViewResolver.resolve(req, "auth/register.jsp").forward(req, resp);
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String csrfToken = req.getParameter(CsrfTokenUtil.CSRF_SESSION_ATTRIBUTE);
+
+    if (!CsrfTokenUtil.validateCsrfToken(req, csrfToken)) {
+      resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid CSRF Token");
+      return;
+    }
     UserService userService = new UserServiceImpl();
     String username = (String) req.getParameter("username");
     String password = (String) req.getParameter("password");
+    String passwordConfirmation = (String) req.getParameter("confirm_password");
+
     User user = new User(username, password, Role.USER);
+    UserValidator userValidator = UserValidator.forUser(user).validateUsername().validatePassword(passwordConfirmation);
+    if (!userValidator.isValid()) {
+      ValidationError validationError = userValidator.getValidationErrors().get(0);
+      req.setAttribute(validationError.getErrorField(),
+          validationError.getMessage());
+      ViewResolver.resolve(req, "auth/register.jsp").forward(req, resp);
+      return;
+    }
     try {
       User createdUser = userService.createUser(user);
       resp.getWriter().println(createdUser.toString());
     } catch (Exception e) {
-      ViewResolver.resolve(req, "errors/error.jsp");
+      resp.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+      return;
     }
 
   }
--- src/main/webapp/WEB-INF/views/auth/register.jsp
+++ src/main/webapp/WEB-INF/views/auth/register.jsp
@@ -4,18 +4,27 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/main.css">
   <title>CruciWeb - Register</title>
 </head>
 
 <body>
   <jsp:include page="../components/nav.jsp" />
   <h1>Register</h1>
-  <form action="register" method="post" id="myForm">
-    <label for="username">Username</label>
+  <form action="register" method="post">
+    ${username_error}<br>
+    <label for="username">Nom d'utilsiateur</label>
     <input type="text" name="username" id="username" required>
-    <label for="password">Password</label>
+    ${password_error}<br>
+    <label for="password">Mot de passe</label>
     <input type="password" name="password" id="password" required>
-    <button type="submit" onclick="submit()">Register</button>
+    ${password_confirmation_error}<br>
+    <label for="confirm_password">Confirmer mot de passe</label>
+    <input type="password" name="confirm_password" id="confirm_password" required>
+
+    <input type="hidden" name="csrfToken" value="${csrfToken}" />
+
+    <button type="submit">Register</button>
   </form>
   <p>Already have an account? <a href="login">Login</a></p>
 </body>
