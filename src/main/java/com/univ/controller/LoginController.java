c29459e3d84f4187c9e2f548960b2e76e57ea843
Nabil-Bil <n.radjai@outlook.fr>
2024-11-24 19:53:24 +0100

    Implement login functionality in LoginController with user validation and session management

--- src/main/java/com/univ/controller/LoginController.java
+++ src/main/java/com/univ/controller/LoginController.java
@@ -2,25 +2,63 @@ package com.univ.controller;
 
 import java.io.IOException;
 
+import com.univ.model.User;
+import com.univ.service.AuthService;
+import com.univ.service.AuthServiceImpl;
+
 import com.univ.util.ViewResolver;
+import com.univ.validator.UserValidator;
+import com.univ.validator.ValidationError;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import com.univ.util.Routes;
 
-@WebServlet("/login")
+@WebServlet(Routes.LOGIN_ROUTE)
 public class LoginController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.setContentType("text/html");
+
     ViewResolver.resolve(req, "auth/login.jsp").forward(req, resp);
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+
+    String username = (String) req.getParameter("username");
+    String password = (String) req.getParameter("password");
+
+    User user = new User(username, password);
+    UserValidator userValidator = UserValidator.forUser(user).validateUsername().validatePassword();
+    if (!userValidator.isValid()) {
+      ValidationError validationError = userValidator.getValidationErrors().get(0);
+      req.setAttribute(validationError.getErrorField(),
+          validationError.getMessage());
+      ViewResolver.resolve(req, "auth/login.jsp").forward(req, resp);
+      return;
+    }
+    try {
+      AuthService authService = new AuthServiceImpl();
+      HttpSession httpSession = req.getSession(true);
+      try {
+        authService.login(username, password, httpSession);
+        resp.sendRedirect("dashboard");
+
+      } catch (Exception e) {
+        resp.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+
+        // req.setAttribute("user_notfound", "Utilisateur Inexistant");
+        // ViewResolver.resolve(req, "auth/login.jsp").forward(req, resp);
+      }
+    } catch (Exception e) {
+      resp.sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+      return;
+    }
 
   }
 }
