e1883d52e1f9b6863e07450eda124e9d6d3aaa23
Nabil-Bil <n.radjai@outlook.fr>
2024-12-01 22:50:29 +0100

    Refactor LoginController and RegisterController for improved readability and structure

--- src/main/java/com/univ/controller/LoginController.java
+++ src/main/java/com/univ/controller/LoginController.java
@@ -1,55 +1,53 @@
 package com.univ.controller;
 
-import java.io.IOException;
-
 import com.univ.model.User;
 import com.univ.service.AuthService;
 import com.univ.service.AuthServiceImpl;
-
+import com.univ.util.Routes;
 import com.univ.util.ViewResolver;
 import com.univ.validator.AuthValidator;
 import com.univ.validator.ValidationError;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import com.univ.util.Routes;
+
+import java.io.IOException;
 
 @WebServlet(Routes.LOGIN_ROUTE)
 public class LoginController extends HttpServlet {
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    ViewResolver.resolve(req, "auth/login.jsp").forward(req, resp);
-  }
-
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-
-    String username = (String) req.getParameter("username");
-    String password = (String) req.getParameter("password");
-
-    User user = new User(username, password);
-
-    try {
-      AuthService authService = new AuthServiceImpl();
-      HttpSession httpSession = req.getSession(true);
-      AuthValidator authValidator = authService.login(user, httpSession);
-      if (authValidator.isNotValid()) {
-        ValidationError validationError = authValidator.getValidationErrors().get(0);
-        req.setAttribute(validationError.getErrorField(),
-            validationError.getMessage());
         ViewResolver.resolve(req, "auth/login.jsp").forward(req, resp);
-      } else {
-        resp.sendRedirect("dashboard");
-      }
-    } catch (Exception e) {
-      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-
     }
-  }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String username = (String) req.getParameter("username");
+        String password = (String) req.getParameter("password");
+
+        User user = new User(username, password);
+
+        try {
+            AuthService authService = new AuthServiceImpl();
+            HttpSession httpSession = req.getSession(true);
+            AuthValidator authValidator = authService.login(user, httpSession);
+            if (authValidator.isNotValid()) {
+                ValidationError validationError = authValidator.getValidationErrors().get(0);
+                req.setAttribute(validationError.getErrorField(),
+                        validationError.getMessage());
+                ViewResolver.resolve(req, "auth/login.jsp").forward(req, resp);
+            } else {
+                resp.sendRedirect(req.getContextPath() + Routes.DASHBOARD_ROUTE);
+            }
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+        }
+    }
 }
--- src/main/java/com/univ/controller/RegisterController.java
+++ src/main/java/com/univ/controller/RegisterController.java
@@ -1,16 +1,14 @@
 package com.univ.controller;
 
-import java.io.IOException;
-
 import com.univ.enums.Role;
 import com.univ.model.User;
-
 import com.univ.service.UserService;
 import com.univ.service.UserServiceImpl;
+import com.univ.util.Routes;
+import com.univ.util.SessionManager;
 import com.univ.util.ViewResolver;
 import com.univ.validator.UserValidator;
 import com.univ.validator.ValidationError;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,43 +16,42 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import com.univ.util.Routes;
-import com.univ.util.SessionManager;
+import java.io.IOException;
 
 @WebServlet(Routes.REGISTER_ROUTE)
 public class RegisterController extends HttpServlet {
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    ViewResolver.resolve(req, "auth/register.jsp").forward(req, resp);
-  }
-
-  @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    UserService userService = new UserServiceImpl();
-    String username = (String) req.getParameter("username");
-    String password = (String) req.getParameter("password");
-    String passwordConfirmation = (String) req.getParameter("confirm_password");
-
-    User user = new User(username, password, Role.USER);
-    try {
-      UserValidator userValidator = userService.createUser(user, passwordConfirmation);
-      if (userValidator.isNotValid()) {
-        ValidationError validationError = userValidator.getValidationErrors().get(0);
-        req.setAttribute(validationError.getErrorField(),
-            validationError.getMessage());
         ViewResolver.resolve(req, "auth/register.jsp").forward(req, resp);
-      } else {
-        HttpSession session = req.getSession(true);
-        SessionManager sessionManager = new SessionManager(session);
-        sessionManager.setLoggedinUser(user.getId(), user.getRole());
-        String dashboardURI = req.getContextPath().concat(Routes.DASHBOARD_ROUTE);
-        resp.sendRedirect(dashboardURI);
-      }
-    } catch (Exception e) {
-      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
 
-  }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        UserService userService = new UserServiceImpl();
+        String username = (String) req.getParameter("username");
+        String password = (String) req.getParameter("password");
+        String passwordConfirmation = (String) req.getParameter("confirm_password");
+
+        User user = new User(username, password, Role.USER);
+        try {
+            UserValidator userValidator = userService.createUser(user, passwordConfirmation);
+            if (userValidator.isNotValid()) {
+                ValidationError validationError = userValidator.getValidationErrors().get(0);
+                req.setAttribute(validationError.getErrorField(),
+                        validationError.getMessage());
+                ViewResolver.resolve(req, "auth/register.jsp").forward(req, resp);
+            } else {
+                HttpSession session = req.getSession(true);
+                SessionManager sessionManager = new SessionManager(session);
+                sessionManager.setLoggedinUser(user.getId(), user.getRole());
+                String dashboardURI = req.getContextPath().concat(Routes.DASHBOARD_ROUTE);
+                resp.sendRedirect(dashboardURI);
+            }
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+    }
 }
