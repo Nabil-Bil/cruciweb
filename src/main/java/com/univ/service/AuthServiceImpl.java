e885fd8dca3566ce5bbd3b8e5bbbe975a09881f1
Nabil-Bil <n.radjai@outlook.fr>
2024-12-02 21:32:41 +0100

    Refactor AuthServiceImpl to improve code readability and update AuthValidator usage in login method

--- src/main/java/com/univ/service/AuthServiceImpl.java
+++ src/main/java/com/univ/service/AuthServiceImpl.java
@@ -1,40 +1,39 @@
 package com.univ.service;
 
-import java.util.Optional;
-
 import com.univ.model.User;
 import com.univ.repository.UserRepository;
 import com.univ.repository.UserRepositoryImpl;
 import com.univ.util.SessionManager;
 import com.univ.validator.AuthValidator;
-
 import jakarta.servlet.http.HttpSession;
+
+import java.util.Optional;
 
 public class AuthServiceImpl implements AuthService {
 
-  private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-  public AuthServiceImpl() {
-    this.userRepository = new UserRepositoryImpl();
-  }
-
-  @Override
-  public AuthValidator login(User user, HttpSession session) throws Exception {
-    SessionManager sessionManager = new SessionManager(session);
-    Optional<User> optionalUser = this.userRepository.findByUsername(user.getUsername());
-    AuthValidator validator = AuthValidator.forUser(user, optionalUser);
-    validator.validateUsername().validatePassword();
-    if (validator.isValid())
-      sessionManager.setLoggedinUser(optionalUser.get().getId(), optionalUser.get().getRole());
-
-    return validator;
-  }
-
-  @Override
-  public void logout(HttpSession session) {
-    if (session != null) {
-      session.invalidate();
+    public AuthServiceImpl() {
+        this.userRepository = new UserRepositoryImpl();
     }
-  }
+
+    @Override
+    public AuthValidator login(User user, HttpSession session) throws Exception {
+        SessionManager sessionManager = new SessionManager(session);
+        Optional<User> optionalUser = this.userRepository.findByUsername(user.getUsername());
+        AuthValidator validator = AuthValidator.of(user, optionalUser);
+        validator.validateUsername().validatePassword();
+        if (validator.isValid())
+            sessionManager.setLoggedinUser(optionalUser.get().getId(), optionalUser.get().getRole());
+
+        return validator;
+    }
+
+    @Override
+    public void logout(HttpSession session) {
+        if (session != null) {
+            session.invalidate();
+        }
+    }
 
 }
