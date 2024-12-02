7f378898cd3258446d3f1d213b43f2f3a04b7ef8
Nabil-Bil <n.radjai@outlook.fr>
2024-12-02 21:33:13 +0100

    Add GRID_ROUTE constant to Routes for grid management

--- src/main/java/com/univ/util/Routes.java
+++ src/main/java/com/univ/util/Routes.java
@@ -1,15 +1,16 @@
 package com.univ.util;
 
 public class Routes {
-  public static final String HOME_ROUTE = "/";
-  public static final String LOGIN_ROUTE = "/login";
-  public static final String REGISTER_ROUTE = "/register";
-  public static final String LOGOUT_ROUTE = "/logout";
-  public static final String DASHBOARD_ROUTE = "/dashboard";
-  public static final String USERS_ROUTE = "/users";
+    public static final String HOME_ROUTE = "/";
+    public static final String LOGIN_ROUTE = "/login";
+    public static final String REGISTER_ROUTE = "/register";
+    public static final String LOGOUT_ROUTE = "/logout";
+    public static final String DASHBOARD_ROUTE = "/dashboard";
+    public static final String USERS_ROUTE = "/users";
+    public static final String GRID_ROUTE = "/grids";
 
-  private Routes() {
-    throw new UnsupportedOperationException("Routes class cannot be instantiated");
-  }
+    private Routes() {
+        throw new UnsupportedOperationException("Routes class cannot be instantiated");
+    }
 
 }
