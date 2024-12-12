49d99d684691a0cf9a6ffbf918cc5f40abe07688
Nabil-Bil <n.radjai@outlook.fr>
2024-12-13 00:15:19 +0100

    Enhance grid creation interface with layout adjustments, form updates, and improved styling

--- src/main/webapp/WEB-INF/views/grids/create.jsp
+++ src/main/webapp/WEB-INF/views/grids/create.jsp
@@ -1,3 +1,4 @@
+<%@ page import="com.univ.util.Routes" %>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -11,48 +12,67 @@
 </head>
 <body>
 <jsp:include page="../components/nav.jsp"/>
-<div class="container">
+<form class="container" id="grid-form" method="post"
+      action="<%=request.getContextPath().concat(Routes.CREATE_GRID_ROUTE)%>">
     <div class="grid-display">
         <h2>Prévisualisation de la Grille</h2>
         <span class="message">
           Cliquez avec le bouton droit sur une case pour la noircir. Cliquez à
           nouveau avec le bouton droit pour la remettre à la normale.
         </span>
-        <div class="grid-wrapper">
-            <div class="column-indices" id="column-indices"></div>
-            <div class="grid-container">
-                <div class="row-indices" id="row-indices"></div>
-                <div class="grid" id="grid"></div>
+        <span class="message">
+          Dans chaque ligne/colonne, séparez les indices par des tirets (-).
+        </span>
+        <div class="crossword-wrapper">
+            <div class="grid-wrapper">
+                <div class="column-indices" id="column-indices"></div>
+                <div class="grid-container">
+                    <div class="row-indices" id="row-indices"></div>
+                    <div class="grid" id="grid"></div>
+                </div>
+            </div>
+            <div class="clues-wrapper">
+                <div class="clues">
+                    <h3>Indices Horizontaux</h3>
+                    <div class="clues-list" id="horizontal-clues"></div>
+                </div>
+                <div class="clues">
+                    <h3>Indices Verticaux</h3>
+                    <div class="clues-list" id="vertical-clues"></div>
+                </div>
             </div>
         </div>
+
     </div>
     <div class="grid-settings">
         <h2>Paramètres de la Grille</h2>
         <div class="form-group">
             <label for="grid-name">Nom de la Grille :</label>
-            <input type="text" id="grid-name" required/>
+            <input type="text" id="grid-name" name="name" required/>
         </div>
         <div class="form-group">
             <label for="grid-width">Largeur (Colonnes) :</label>
-            <input type="number" id="grid-width" value="6" min="6" max="26"/>
+            <input type="number" name="width" id="grid-width" value="6" min="6" max="26"/>
         </div>
         <div class="form-group">
             <label for="grid-height">Hauteur (Lignes) :</label>
-            <input type="number" id="grid-height" value="6" min="6" max="26"/>
+            <input type="number" name="height" id="grid-height" value="6" min="6" max="26"/>
         </div>
         <div class="form-group">
             <label for="grid-difficulty">Difficulté :</label>
-            <select id="grid-difficulty">
+            <select id="grid-difficulty" name="difficulty">
                 <option value="easy">Facile</option>
                 <option value="medium">Moyen</option>
                 <option value="hard">Difficile</option>
             </select>
         </div>
         <div class="form-group-buttons">
+            <input type="hidden" name="csrfToken" value="${csrfToken}"/>
+            <input type="hidden" id="gridMatrixData" name="gridMatrixData">
             <button id="reset-button" class="button">Réinitialiser</button>
-            <button id="save-button" class="button">Créer</button>
+            <button id="save-button" type="submit" class="button">Créer</button>
         </div>
     </div>
-</div>
+</form>
 </body>
 </html>
--- src/main/webapp/resources/js/createGrid.js
+++ src/main/webapp/resources/js/createGrid.js
@@ -1,91 +1,166 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const gridElement = document.getElementById("grid");
-    const columnIndicesElement = document.getElementById("column-indices");
-    const rowIndicesElement = document.getElementById("row-indices");
-    const gridNameInput = document.getElementById("grid-name");
-    const gridDifficultyInput = document.getElementById("grid-difficulty");
+    const MIN_GRID_SIZE = 6;
+    const MAX_GRID_SIZE = 26;
+    const CELL_SIZE = 30;
+    const BLACK_CELLS_COLOR = "white";
+    const elements = {
+        gridElement: document.getElementById("grid"),
+        columnIndicesElement: document.getElementById("column-indices"),
+        rowIndicesElement: document.getElementById("row-indices"),
+        gridNameInput: document.getElementById("grid-name"),
+        gridDifficultyInput: document.getElementById("grid-difficulty"),
+        gridMatrixData: document.getElementById("gridMatrixData"),
+        widthInput: document.getElementById("grid-width"),
+        heightInput: document.getElementById("grid-height"),
+        resetButton: document.getElementById("reset-button"),
+        csrfToken: document.getElementById("csrfToken"),
+        form: document.getElementById("grid-form"),
+        horizontalClues: document.getElementById("horizontal-clues"),
+        verticalClues: document.getElementById("vertical-clues")
+    };
 
-    const widthInput = document.getElementById("grid-width");
-    const heightInput = document.getElementById("grid-height");
-    const resetButton = document.getElementById("reset-button");
-    const saveButton = document.getElementById("save-button");
     let selectedCell = null;
     let gridMatrix = [];
 
     function generateGrid(width, height) {
+        const {gridElement, columnIndicesElement, rowIndicesElement, verticalClues, horizontalClues} = elements;
+
         gridElement.innerHTML = "";
         columnIndicesElement.innerHTML = "";
         rowIndicesElement.innerHTML = "";
-        gridMatrix = Array.from({length: height}, () => Array(width).fill(""));
+        verticalClues.innerHTML = "";
+        horizontalClues.innerHTML = "";
+        gridMatrix = Array.from({length: height}, () => Array(width).fill(" "));
 
         for (let i = 0; i < width; i++) {
-            const letter = String.fromCharCode(65 + i);
             const columnIndex = document.createElement("span");
-            columnIndex.textContent = letter;
+            let letter = String.fromCharCode(65 + i);
+            columnIndex.textContent = letter
             columnIndicesElement.appendChild(columnIndex);
+            const verticalClue = document.createElement("span");
+            verticalClue.classList.add("clue");
+            verticalClue.textContent = letter;
+            const verticalClueInput = document.createElement("input");
+            verticalClueInput.type = "text";
+            verticalClueInput.name = `clue-column-${i}`;
+            verticalClue.appendChild(verticalClueInput);
+            verticalClues.appendChild(verticalClue);
         }
 
-        gridElement.style.gridTemplateColumns = `repeat(${width}, 30px)`;
+        gridElement.style.gridTemplateColumns = `repeat(${width}, ${CELL_SIZE}px)`;
 
         for (let i = 0; i < height; i++) {
             const rowIndex = document.createElement("span");
             rowIndex.textContent = i + 1;
             rowIndicesElement.appendChild(rowIndex);
+            const horizontalClue = document.createElement("span");
+            horizontalClue.classList.add("clue");
+            horizontalClue.textContent = i + 1;
+            const horizontalClueInput = document.createElement("input");
+            horizontalClueInput.type = "text";
+            horizontalClueInput.name = `clue-row-${i}`;
+            horizontalClue.appendChild(horizontalClueInput);
+            horizontalClues.appendChild(horizontalClue);
 
             for (let j = 0; j < width; j++) {
-                const gridCell = document.createElement("div");
-                gridCell.classList.add("grid-cell");
-
-                gridCell.addEventListener("click", () => {
-                    if (gridCell.style.backgroundColor === "black") {
-                        return;
-                    }
-                    if (selectedCell) {
-                        selectedCell.classList.remove("selected");
-                    }
-                    if (selectedCell === gridCell) {
-                        selectedCell = null;
-                        return;
-                    }
-
-                    selectedCell = gridCell;
-                    selectedCell.classList.add("selected");
-                });
-
-                gridCell.dataset.row = i;
-                gridCell.dataset.col = j;
-
+                const gridCell = createGridCell(i, j);
                 gridElement.appendChild(gridCell);
             }
         }
     }
 
-    resetButton.addEventListener("click", () => {
-        gridNameInput.value = "";
-        widthInput.value = 6;
-        heightInput.value = 6;
-        gridDifficultyInput.value = "easy";
-        generateGrid(6, 6);
-    });
+    function createGridCell(row, col) {
+        const gridCell = document.createElement("div");
+        gridCell.classList.add("grid-cell");
+        gridCell.dataset.row = row;
+        gridCell.dataset.col = col;
 
-    saveButton.addEventListener("click", () => {
-        if (gridNameInput.checkValidity() === false) {
-            gridNameInput.reportValidity();
+        gridCell.addEventListener("click", () => handleCellClick(gridCell));
+        gridCell.addEventListener("contextmenu", (event) => handleCellRightClick(event, gridCell));
+
+        return gridCell;
+    }
+
+    function handleCellClick(cell) {
+        if (cell.style.backgroundColor === BLACK_CELLS_COLOR) {
+            selectedCell = null;
             return;
         }
 
-        const gridData = {
-            name: gridNameInput.value,
-            difficulty: gridDifficultyInput.value,
-            width: parseInt(widthInput.value, 10),
-            height: parseInt(heightInput.value, 10),
-            grid: gridMatrix,
-        };
-        console.log(gridData);
-    });
+        if (selectedCell) selectedCell.classList.remove("selected");
 
-    document.addEventListener("keydown", (event) => {
-        if (selectedCell) {
+        if (selectedCell === cell) {
+            selectedCell = null;
+        } else {
+            selectedCell = cell;
+            cell.classList.add("selected");
+        }
+    }
+
+    function handleCellRightClick(event, cell) {
+        event.preventDefault();
+        if (selectedCell === cell) {
+            selectedCell.classList.remove("selected")
+            selectedCell = null;
+        }
+        const row = parseInt(cell.dataset.row, 10);
+        const col = parseInt(cell.dataset.col, 10);
+
+        if (gridMatrix[row][col] === "*") {
+            gridMatrix[row][col] = " ";
+            cell.style.backgroundColor = "";
+            cell.textContent = "";
+        } else {
+            gridMatrix[row][col] = "*";
+            cell.style.backgroundColor = BLACK_CELLS_COLOR;
+            cell.textContent = "";
+        }
+    }
+
+    function resetGrid() {
+        const {gridNameInput, widthInput, heightInput, gridDifficultyInput} = elements;
+        gridNameInput.value = "";
+        widthInput.value = MIN_GRID_SIZE;
+        heightInput.value = MIN_GRID_SIZE;
+        gridDifficultyInput.value = "easy";
+        generateGrid(MIN_GRID_SIZE, MIN_GRID_SIZE);
+    }
+
+    function updateGridSize() {
+        const {widthInput, heightInput} = elements;
+
+        const width = clamp(parseInt(widthInput.value, 10) || MIN_GRID_SIZE, MIN_GRID_SIZE, MAX_GRID_SIZE);
+        const height = clamp(parseInt(heightInput.value, 10) || MIN_GRID_SIZE, MIN_GRID_SIZE, MAX_GRID_SIZE);
+
+        widthInput.value = width;
+        heightInput.value = height;
+
+        generateGrid(width, height);
+    }
+
+    function clamp(value, min, max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    function initializeEvents() {
+        const {resetButton, widthInput, heightInput, form} = elements;
+        form.addEventListener("submit", (event) => {
+            elements.gridMatrixData.value = JSON.stringify(gridMatrix);
+            if (!form.checkValidity()) {
+                form.reportValidity();
+                return;
+            }
+            form.submit();
+        });
+
+        resetButton.addEventListener("click", resetGrid);
+
+        widthInput.addEventListener("input", updateGridSize);
+        heightInput.addEventListener("input", updateGridSize);
+
+        document.addEventListener("keydown", (event) => {
+            if (!selectedCell) return;
+
             const row = parseInt(selectedCell.dataset.row, 10);
             const col = parseInt(selectedCell.dataset.col, 10);
 
@@ -93,57 +168,89 @@ document.addEventListener("DOMContentLoaded", () => {
                 const value = event.key.toUpperCase();
                 selectedCell.textContent = value;
                 gridMatrix[row][col] = value;
+
+                let nextRow = row;
+                let nextCol = col + 1;
+
+                if (nextCol >= gridMatrix[0].length) {
+                    nextCol = 0;
+                    nextRow = row + 1;
+                }
+
+                if (nextRow < gridMatrix.length) {
+                    let nextCell = document.querySelector(`[data-row='${nextRow}'][data-col='${nextCol}']`);
+
+                    while (nextCell && gridMatrix[nextRow][nextCol] === "*") {
+                        nextCol += 1;
+
+                        if (nextCol >= gridMatrix[0].length) {
+                            nextCol = 0;
+                            nextRow = row + 1;
+                        }
+
+                        if (nextRow >= gridMatrix.length) {
+                            return;
+                        }
+
+                        nextCell = document.querySelector(`[data-row='${nextRow}'][data-col='${nextCol}']`);
+                    }
+
+                    if (nextCell) {
+                        selectedCell.classList.remove("selected");
+                        nextCell.classList.add("selected");
+                        selectedCell = nextCell;
+                    }
+                }
             } else if (event.key === "Backspace") {
-                selectedCell.textContent = "";
-                gridMatrix[row][col] = "";
-            }
-        }
-    });
+                if (selectedCell.textContent !== "") {
+                    selectedCell.textContent = "";
+                    gridMatrix[row][col] = " ";
+                } else {
+                    let prevRow = row;
+                    let prevCol = col - 1;
 
-    gridElement.addEventListener("contextmenu", (event) => {
-        event.preventDefault();
-        if (event.target.classList.contains("grid-cell")) {
-            const row = parseInt(event.target.dataset.row, 10);
-            const col = parseInt(event.target.dataset.col, 10);
-            if (selectedCell === event.target) {
-                selectedCell.classList.remove("selected");
+                    if (prevCol < 0) {
+                        prevCol = gridMatrix[0].length - 1;
+                        prevRow = row - 1;
+                    }
+
+                    if (prevRow >= 0) {
+                        let prevCell = document.querySelector(`[data-row='${prevRow}'][data-col='${prevCol}']`);
+
+                        while (prevCell && gridMatrix[prevRow][prevCol] === "*") {
+                            prevCol -= 1;
+
+                            if (prevCol < 0) {
+                                prevCol = gridMatrix[0].length - 1;
+                                prevRow -= 1;
+                            }
+
+                            if (prevRow < 0) {
+                                return;
+                            }
+
+                            prevCell = document.querySelector(`[data-row='${prevRow}'][data-col='${prevCol}']`);
+                        }
+
+                        if (prevCell) {
+                            selectedCell.classList.remove("selected");
+                            prevCell.classList.add("selected");
+                            selectedCell = prevCell;
+                        }
+                    }
+                }
+            }
+        });
+
+        document.addEventListener("click", (event) => {
+            if (!elements.gridElement.contains(event.target)) {
                 selectedCell = null;
+                const allCells = document.querySelectorAll(".grid-cell");
+                allCells.forEach(cell => cell.classList.remove("selected"));
             }
-            if (gridMatrix[row][col] === "*") {
-                gridMatrix[row][col] = "";
-                event.target.style.backgroundColor = "";
-                event.target.textContent = "";
-            } else {
-                gridMatrix[row][col] = "*";
-                event.target.style.backgroundColor = "black";
-                event.target.textContent = "";
-            }
-        }
-    });
+        });
+    }
 
-    widthInput.addEventListener("input", () => {
-        if (!/^\d*$/.test(widthInput.value)) {
-            widthInput.value = widthInput.value.replace(/\D/g, "");
-        }
-        if (widthInput.value === "") {
-            widthInput.value = 6;
-        }
-        const width = Math.min(Math.max(parseInt(widthInput.value, 10), 6), 26);
-        widthInput.value = width;
-        generateGrid(width, parseInt(heightInput.value, 10));
-    });
-
-    heightInput.addEventListener("input", () => {
-        if (!/^\d*$/.test(heightInput.value)) {
-            heightInput.value = heightInput.value.replace(/\D/g, "");
-        }
-        if (heightInput.value === "") {
-            heightInput.value = 6;
-        }
-        const height = Math.min(Math.max(parseInt(heightInput.value, 10), 6), 26);
-        heightInput.value = height;
-        generateGrid(parseInt(widthInput.value, 10), height);
-    });
-
-    generateGrid(parseInt(widthInput.value, 10), parseInt(heightInput.value, 10));
+    initializeEvents();
+    resetGrid();
 });
