6802b8514081772f86a0f469705a2518ddb61cbb
Nabil-Bil <n.radjai@outlook.fr>
2025-01-01 00:18:18 +0100

    Rename ValidationError to ValidationResult and add validation status methods; update web.xml to enable 500 error page

--- src/main/java/com/univ/validator/ValidationError.java
+++ src/main/java/com/univ/validator/ValidationError.java
@@ -1,23 +1,31 @@
 package com.univ.validator;
 
-public class ValidationError {
-  private String field;
-  private String message;
+public class ValidationResult {
+    private String field;
+    private String message;
 
-  public ValidationError(String field, String message) {
-    this.field = field;
-    this.message = message;
-  }
+    public ValidationResult(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
 
-  public String getField() {
-    return field;
-  }
+    public String getField() {
+        return field;
+    }
 
-  public String getErrorField() {
-    return field.concat("_error");
-  }
+    public String getErrorField() {
+        return field.concat("_error");
+    }
 
-  public String getMessage() {
-    return message;
-  }
+    public String getMessage() {
+        return message;
+    }
+    
+    public boolean isValid() {
+        return validationResults.isEmpty();
+    }
+
+    public boolean isNotValid() {
+        return !this.isValid();
+    }
 }
